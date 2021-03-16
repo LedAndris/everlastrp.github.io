39756b91ff5edcc28aa6a9e173206c128308b307
LedAndris <ledererandras2004@gmail.com>
2021-03-16 08:40:14 +0100

    tweak(css): More css design, refactor

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import styles from '../styles/Error.module.css'
+import {btn} from '../styles/Global.module.css'
+
 const NotFound = () => {
     return (
         <div className="not-found">
             <div>
                 <img src="https://beta.iodine.gg/gg80b.webp" width="1000px"></img>
-                <Link href="/">
-                <button className={styles.btn}>Go home!</button>
+                <Link href="/" target="_blank">
+                <button className={btn}>Go home!</button>
                 </Link>
             </div>
         </div>
